467ae65497f3c16ff13187e6affc442cdc969d81
unknown <rogeriompessoas@gmail.com>
2023-06-14 08:25:19 -0300

    rodape

--- src/App.jsx
+++ src/App.jsx
@@ -11,27 +11,17 @@ function App() {
 
   return (
     <BrowserRouter>
-    <nav>
-          <ul>
-            <li>
-              <Link to='/Contato'>Contato</Link>
-            </li>
-            <li>
-              <Link to='/Home'>Home</Link>
-            </li>
-            <li>
-              <Link to='Novo'>Novo</Link>
-            </li>
-          </ul>
-        </nav>
-
+    
+   
+    
         <Routes>
-          <Route path='/' element={<Inicio/>} />
+          <Route path='/' element={<Home/>} />
           <Route path='/Contato' element={<Contato />} />
           <Route path='/Home' element={<Home/>} />
           <Route path='/Novo' element={<Novo/>} />
 
         </Routes>
+        
     </BrowserRouter>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,28 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
-import Home from './Components/Pages/Home.jsx'
-import Contato from './Components/Pages/Contato.jsx'
-import Novo from './Components/Pages/Novo.jsx'
-
-const router =createBrowserRouter([
-  {
-    path:"/",
-    element: <Home/>
-  },
-  {
-    path:"/Contato",
-    element:<Contato />,
-  },
-  {
-    path:"Novo",
-    element:<Novo />,
-  }
-]);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-);
+    <App/>
+  </React.StrictMode>
+)
